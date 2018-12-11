b3446e726158ef1b3095b39df38154ca86d8d758
mmartinsolis <mmartinsolis.dev@gmail.com>
2018-12-11 18:17:54 -0500

    Se creó el servidor backend y se añadieron las pantallas del diseño de la interfaz

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+'use strict';
+const express = require('express');
+//const bodyParser = require('body-parser');
+//const cors = require('cors');
+const mongoose = require('mongoose');
+//const path = require('path');
+const config = require('./config/db');
+//var api = require('./routes/routes');
+
+const app = express();
+
+// Conexion a la base de datos
+mongoose.connect(config.database, { useNewUrlParser: true }).then(
+    () => {
+        console.log('Conectado a la base de datos ' + config.database)
+        app.listen(port, () => {
+            console.log("Servidor escuchando en puerto " + port);
+        });
+    },
+    err => { console.log('Error de conexion ' + err) }
+);
+
+
+// Usar este puerto si la app será alojada en Heroku
+var port = process.env.PORT || 8080;
+// Usar este puerto para desarrollo
+// const port = 3000;
+
+// CORS Middleware permite la conexion entre archivos
+//app.use(cors());
+
+// Establecer carpeta
+//app.use(express.static(path.join(__dirname, 'public')))
+
+// Body Parser Middleware permite la renderización de las vistas
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+
+//Headers necesarios para que el servidor node de accso a las peticiones no importando que cabeceras
+/*app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });*/
+
+//app.use('/api', api);
---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+'use strict';
+module.exports = {
+    database: 'mongodb://dev:devcross2018@ds225294.mlab.com:25294/crossfitapp',
+    secret: 'crossapp'
+}
